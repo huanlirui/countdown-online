3fc40d73f46d7e21e3be7f1de1443dcc309616d9
huanlirui <huanlirui@gmail.com>
2025-01-07 17:51:09 +0800

    feat: seo optimization

---
+++ app/components/JsonLd.tsx
@@ -0,0 +1,27 @@
+export default function JsonLd() {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "WebApplication",
+          "name": "倒计时工具",
+          "description": "免费在线倒计时工具，支持多个倒计时同时进行，精确到秒",
+          "url": "https://www.countdown-online.com",
+          "applicationCategory": "工具软件",
+          "operatingSystem": "All",
+          "offers": {
+            "@type": "Offer",
+            "price": "0",
+            "priceCurrency": "CNY"
+          },
+          "author": {
+            "@type": "Person",
+            "name": "ynxh666"
+          }
+        })
+      }}
+    />
+  );
+} 
